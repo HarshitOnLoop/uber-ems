b1af1d9b6e18afaae86f30e989bed0c0b4b6869f
I'm Harshit Sharma <143705769+HarshitOnLoop@users.noreply.github.com>
2026-03-03 20:00:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
-
+const path = require('path');
 // when running locally we start the same express app
 const app = require("./api");
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.get("*", (req, res) => {
+  
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
